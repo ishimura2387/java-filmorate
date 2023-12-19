2ce19cff764883d4cb2ea256431034999f8a96e5
Shestakov_Denis <shestakov_denis_job@mail.ru>
2023-12-19 19:47:01 +0800

    for check 10 sprint: v.1.1.

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.NullObjectException;
-import ru.yandex.practicum.filmorate.model.Film;;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.service.UserService;
 
